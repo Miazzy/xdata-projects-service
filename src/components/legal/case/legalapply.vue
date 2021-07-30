dd8c6e03aabfd8f452570bbda8a9ee6f0dd82196
yunwisdom <qq695926920@163.com>
2021-07-30 11:25:49 +0800

    init

--- src/components/legal/case/legalapply.vue
+++ src/components/legal/case/legalapply.vue
@@ -2698,7 +2698,10 @@ export default {
       async handleProcess(){
         this.loading = true; // 显示加载状态
         const userinfo = await Betools.storage.getStore('system_userinfo'); // 获取用户基础信息
-        const id = Betools.tools.getUrlParam('id'); // 表单ID
+        const pid = Betools.tools.getUrlParam('id'); // 表单ID
+        const id = Betools.tools.queryUniqueID(); // 表单ID
+        const create_time = dayjs().format('YYYY-MM-DD');
+        const create_by = (userinfo ? userinfo.realname || userinfo.name || userinfo.lastname : '');
 
         // 验证数据是否已经填写
         if(Betools.tools.isNull(this.legal.lawcase)){
@@ -2710,15 +2713,14 @@ export default {
             title: "确认操作",
             content: "是否确认追加此案件进展信息?",
             onOk: async() => {
-                  const {legal} = this;
                   const { lawcase } = this.legal;
-                  const result = await Betools.manage.patchTableData(this.tablename, id, {lawcase}); // 向表单提交form对象数据
+                  const element = { id, create_time, pid, create_by, content:lawcase };
+                  const result = await Betools.manage.postTableData('bs_legal_progress', element); // 向表单提交form对象数据
                   if(result && result.error && result.error.errno){ //提交数据如果出现错误，请提示错误信息
                       return await vant.Dialog.alert({  title: '温馨提示',  message: `系统错误，请联系管理人员，错误编码：[${result.error.code}]. `, });
                   }
                   
                   (async()=>{
-                    Betools.manage.sortTableData(this.tablename , 'serialID');
                     Betools.manage.handleLog(this.tablename , legal , '执行' , '案件进展管理' , `${userinfo.realname} 追加了案号为：${legal.caseID}的案件进展，内容：${lawcase}。`);
                   })();
 
