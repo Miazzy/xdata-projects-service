049c094f0343cb2c553d119f0744bc1d57f68682
yunwisdom <qq695926920@163.com>
2021-08-16 15:40:05 +0800

    init

--- src/request/workflow.js
+++ src/request/workflow.js
@@ -215,8 +215,7 @@ export async function postWorkflowApprove(tableName, curRow, operationData, pnod
         if (tableName != null && curRow != null && prLogHisNode != null && bpmStatus != null) {
 
             await Betools.manage.postProcessLogHistory(prLogHisNode); //将当前审批日志转为历史日志，并删除当前审批日志中相关信息
-            await Betools.manage.deleteTableData('pr_log', prLogHisNode.id); //删除当前审批节点中的所有记录
-            await Betools.manage.deleteProcessLog(tableName, prLogHisNode); //删除当前审批节点中的所有记录
+            prLogHisNode.map(async(element) => { await Betools.manage.deleteTableData('pr_log', element.id)}); //删除当前审批节点中的所有记录
 
             //修改审批状态为审批中，并记录审批日志；将当前审批状态修改为处理中
             result = await Betools.manage.patchTableData( tableName, curRow["business_data_id"], bpmStatus );
