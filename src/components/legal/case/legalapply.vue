ebb62db82223798d02b097e84499340fc8ec7914
yunwisdom <qq695926920@163.com>
2021-08-16 11:00:23 +0800

    init

--- src/components/legal/case/legalapply.vue
+++ src/components/legal/case/legalapply.vue
@@ -2706,7 +2706,8 @@ export default {
 
            // 此处推送消息至第一个审批处
            try {
-              const receiveURL = encodeURIComponent(`${window.location.host.includes('localhost') ? `https://legal.yunwisdom.club:30443` : window.location.protocol + '//' + window.location.host }/#/legal/case/legalview?id=${data.id}&bpm_status=2&proponents=${firstWflowUser}`);
+              const curHost = window.location.protocol + '//' + window.location.host;
+              const receiveURL = encodeURIComponent(`${window.location.host.includes('localhost') ? `https://legal.yunwisdom.club:30443` : curHost }/#/legal/case/legalview?id=${data.id}&bpm_status=2&proponents=${firstWflowUser}`);
               await superagent.get(`${window.BECONFIG['restAPI']}/api/v1/weappms/${firstWflowUser}/您好，${userinfo['name']||userinfo['realname']}(${userinfo["username"]})提交了案件发起申请：${data["title"]}}，请您及时进行审批处理！?type=legal&rurl=${receiveURL}`)
                           .set('accept', 'json');
            } catch (error) {
