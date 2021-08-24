858110771267e212ea9663773dbb307fff48d8f7
yunwisdom <qq695926920@163.com>
2021-08-24 10:12:00 +0800

    init

--- src/components/legal/outsource/outsourceapply.vue
+++ src/components/legal/outsource/outsourceapply.vue
@@ -757,7 +757,7 @@ export default {
             const processID = Betools.tools.getUrlParam('processID');
             const domainURL = 'https://legal.yunwisdom.club:30443';
             response = await workprocess.handleAgreeWF(this.tablename, this.legal.id, this.legal, this.workflow.content, processID , '', domainURL);
-            this.$router.push(`/legal/case/legalapply?id=${this.legal.id}&type=1&tname=案件详情&apply=view&role=view`);
+            this.$router.push(`/legal/${this.tablename.replace('bs_legal_','') + 'apply'}?id=${this.legal.id}&type=1&tname=流程详情&apply=view&role=view`);
             this.processLogList = await Betools.query.queryProcessLog();
             this.role = this.apply = 'view';
             vant.Toast.clear();
@@ -777,7 +777,7 @@ export default {
             const processID = Betools.tools.getUrlParam('processID');
             const domainURL = 'https://legal.yunwisdom.club:30443';
             response = await workprocess.handleRejectWF(this.tablename, this.legal.id, this.legal, this.workflow.content, processID, '', domainURL);
-            this.$router.push(`/legal/case/legalapply?id=${this.legal.id}&type=1&tname=案件详情&apply=view&role=view`);
+            this.$router.push(`/legal/${this.tablename.replace('bs_legal_','') + 'apply'}?id=${this.legal.id}&type=1&tname=流程详情&apply=view&role=view`);
             this.processLogList = await Betools.query.queryProcessLog();
             this.role = this.apply = 'view';
             vant.Toast.clear();
