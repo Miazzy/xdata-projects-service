bdae8b5cf8cd3987c73a7ee7828b5f6255c84141
yunwisdom <qq695926920@163.com>
2021-09-06 16:10:17 +0800

    init

--- src/components/legal/ruling/rulingapply.vue
+++ src/components/legal/ruling/rulingapply.vue
@@ -91,22 +91,10 @@
                 <div class="reward-apply-content-item" style="margin-top:5px;margin-bottom:5px; margin-right:10px;">
                   <a-row>
                     <a-col :span="4" style="font-size:1.0rem; margin-top:5px; text-align: center;">
-                      <span style="position:relative;" ><span style="color:red;margin-right:0px;position:absolute;left:-10px;top:0px;">*</span>起诉/应诉</span>
+                      <span style="position:relative;" ><span style="color:red;margin-right:0px;position:absolute;left:-10px;top:0px;">*</span>文书收取日期    </span>
                     </a-col>
                     <a-col :span="8">
-                      <a-select  v-model="element.case_type" default-value="起诉案件" @blur="validFieldToast('caseSType')"  placeholder="请选择案件(起诉/应诉)类别！" style="width:100%; border: 0px solid #fefefe;  border-bottom: 1px solid #f0f0f0;">
-                        <template v-for="(elem,index) in options.caseSTypeOptions" >
-                          <a-select-option :key="index" :value="elem" >
-                            {{elem}}
-                          </a-select-option>
-                        </template>
-                      </a-select>
-                    </a-col>
-                    <a-col :span="4" style="font-size:1.0rem; margin-top:5px; text-align: center;">
-                      <span style="position:relative;" ><span style="color:red;margin-right:0px;position:absolute;left:-10px;top:0px;"></span>{{ element.case_type == '应诉案件' ? '被' : '' }}保全状态</span>
-                    </a-col>
-                    <a-col :span="8">
-                      <a-auto-complete :data-source="['是','否','待定']" v-model="element.preserve" placeholder="请输入保全/被保全状态！" style="border: 0px solid #fefefe;  border-bottom: 1px solid #f0f0f0; width:100%; border-width: 0px 0px 1px; border-style: solid; border-color: rgb(254, 254, 254) rgb(254, 254, 254) rgb(240, 240, 240); border-image: initial;" :filter-option="filterOption" />
+                      <a-date-picker v-model="element.ruling_time" readonly placeholder="请选择文书收取日期！" style="width:100%; border: 0px solid #fefefe;  border-bottom: 1px solid #f0f0f0;"  />
                     </a-col>
                   </a-row>
                 </div>
@@ -114,104 +102,16 @@
                 <div class="reward-apply-content-item" style="margin-top:5px;margin-bottom:5px; margin-right:10px;">
                   <a-row>
                     <a-col :span="4" style="font-size:1.0rem; margin-top:5px; text-align: center;">
-                      <span style="position:relative;" ><span style="color:red;margin-right:0px;position:absolute;left:-10px;top:0px;">*</span>保全类型</span>
+                      <span style="position:relative;" ><span style="color:red;margin-right:0px;position:absolute;left:-10px;top:0px;"></span>是否上诉</span>
                     </a-col>
                     <a-col :span="8">
-                      <a-auto-complete :data-source="['货币现金','银行账户','债券','土地使用权','房产','车辆','机器设备','债权','股权','股票','基金','知识产权','其他财产']" v-model="element.pretype" placeholder="请输入保全/被保全状态！" style="border: 0px solid #fefefe;  border-bottom: 1px solid #f0f0f0; width:100%; border-width: 0px 0px 1px; border-style: solid; border-color: rgb(254, 254, 254) rgb(254, 254, 254) rgb(240, 240, 240); border-image: initial;" :filter-option="filterOption" />
-                    </a-col>
-                  </a-row>
-                </div>
-
-                <div class="reward-apply-content-item" style="margin-top:5px;margin-bottom:5px; margin-right:10px;">
-                  <a-row>
-                    <a-col :span="4" style="font-size:1.0rem; margin-top:5px; text-align: center;">
-                      <span style="position:relative;" ><span style="color:red;margin-right:0px;position:absolute;left:-10px;top:0px;"></span>保全阶段</span>
-                    </a-col>
-                    <a-col :span="8">
-                      <a-auto-complete :data-source="['诉前','诉中']" v-model="element.prestage" placeholder="请输入保全阶段！" style="border: 0px solid #fefefe;  border-bottom: 1px solid #f0f0f0; width:100%; border-width: 0px 0px 1px; border-style: solid; border-color: rgb(254, 254, 254) rgb(254, 254, 254) rgb(240, 240, 240); border-image: initial;" :filter-option="filterOption" />
+                      <a-auto-complete :data-source="['是','否']" v-model="element.appeal_flag" placeholder="请输入是否上诉！" style="border: 0px solid #fefefe;  border-bottom: 1px solid #f0f0f0; width:100%; border-width: 0px 0px 1px; border-style: solid; border-color: rgb(254, 254, 254) rgb(254, 254, 254) rgb(240, 240, 240); border-image: initial;" :filter-option="filterOption" />
                     </a-col>
                     <a-col :span="4" style="font-size:1.0rem; margin-top:5px; text-align: center;">
-                      <span style="position:relative;" ><span style="color:red;margin-right:0px;position:absolute;left:-10px;top:0px;"></span>保全进度</span>
+                      <span style="position:relative;" ><span style="color:red;margin-right:0px;position:absolute;left:-10px;top:0px;"></span>上诉日期</span>
                     </a-col>
                     <a-col :span="8">
-                      <a-auto-complete :data-source="['待提保全申请审批','保全申请已审批','已用印','待出具保函','已出具保函','保全申请','保函提交法院']" v-model="element.progess" placeholder="请输入保全进度！" style="border: 0px solid #fefefe;  border-bottom: 1px solid #f0f0f0; width:100%; border-width: 0px 0px 1px; border-style: solid; border-color: rgb(254, 254, 254) rgb(254, 254, 254) rgb(240, 240, 240); border-image: initial;" :filter-option="filterOption" />
-                    </a-col>
-                  </a-row>
-                </div>
-
-                <div class="reward-apply-content-item" style="margin-top:5px;margin-bottom:5px; margin-right:10px;">
-                  <a-row>
-                    <a-col :span="4" style="font-size:1.0rem; margin-top:5px; text-align: center;">
-                      <span style="position:relative;" ><span style="color:red;margin-right:0px;position:absolute;left:-10px;top:0px;"></span>解冻时间</span>
-                    </a-col>
-                    <a-col :span="8">
-                      <a-date-picker v-model="element.thaw_time" placeholder="请输入解冻时间！" style="border: 0px solid #fefefe;  border-bottom: 1px solid #f0f0f0;"  />
-                    </a-col>
-                    <a-col :span="4" style="font-size:1.0rem; margin-top:5px; text-align: center;">
-                      <span style="position:relative;" ><span style="color:red;margin-right:0px;position:absolute;left:-10px;top:0px;"></span>解冻金额</span>
-                    </a-col>
-                    <a-col :span="8">
-                      <a-input v-model="element.thaw_amount" placeholder="请输入解冻金额！" style="width:100%; border: 0px solid #fefefe;  border-bottom: 1px solid #f0f0f0; border-width: 0px 0px 1px; border-style: solid; border-color: rgb(254, 254, 254) rgb(254, 254, 254) rgb(240, 240, 240); border-image: initial;" :filter-option="filterOption" />
-                    </a-col>
-                  </a-row>
-                </div>
-
-                <div v-if="element.pretype.includes('银行账户')">
-                <div class="reward-apply-content-item reward-apply-content-title" style="padding-top:5px;">
-                   <a-row style="border-top: 1px dash #f0f0f0;" >
-                    <a-col class="reward-apply-content-title-text" :span="4" style="font-size:1.1rem;">
-                      账户信息
-                    </a-col>
-                   </a-row>
-                </div>
-
-                <div class="reward-apply-content-item" style="margin-top:5px;margin-bottom:5px; margin-right:10px;">
-                  <a-row>
-                    <a-col :span="4" style="font-size:1.0rem; margin-top:5px; text-align: center;">
-                      <span style="position:relative;" ><span style="color:red;margin-right:0px;position:absolute;left:-10px;top:0px;"></span>账户类型</span>
-                    </a-col>
-                    <a-col :span="8">
-                      <a-auto-complete :data-source="['基本账户','工资专户']" v-model="element.account_type" placeholder="请输入账户类型！" style="border: 0px solid #fefefe;  border-bottom: 1px solid #f0f0f0; width:100%; border-width: 0px 0px 1px; border-style: solid; border-color: rgb(254, 254, 254) rgb(254, 254, 254) rgb(240, 240, 240); border-image: initial;" :filter-option="filterOption" />
-                    </a-col>
-                    <a-col :span="4" style="font-size:1.0rem; margin-top:5px; text-align: center;">
-                      <span style="position:relative;" ><span style="color:red;margin-right:0px;position:absolute;left:-10px;top:0px;"></span>是否超额</span>
-                    </a-col>
-                    <a-col :span="8">
-                      <a-auto-complete :data-source="['是','否']" v-model="element.excess" placeholder="请输入是否超额！" style="border: 0px solid #fefefe;  border-bottom: 1px solid #f0f0f0; width:100%; border-width: 0px 0px 1px; border-style: solid; border-color: rgb(254, 254, 254) rgb(254, 254, 254) rgb(240, 240, 240); border-image: initial;" :filter-option="filterOption" />
-                    </a-col>
-                  </a-row>
-                </div>
-
-                <div class="reward-apply-content-item" style="margin-top:5px;margin-bottom:5px; margin-right:10px;">
-                  <a-row>
-                    <a-col :span="4" style="font-size:1.0rem; margin-top:5px; text-align: center;">
-                      <span style="position:relative;" ><span style="color:red;margin-right:0px;position:absolute;left:-10px;top:0px;"></span>超额金额</span>
-                    </a-col>
-                    <a-col :span="8">
-                      <a-input v-model="element.excess_money" readonly placeholder="请输入超额金额！" style="border: 0px solid #fefefe;  border-bottom: 1px solid #f0f0f0;"  />
-                    </a-col>
-                    <a-col :span="4" style="font-size:1.0rem; margin-top:5px; text-align: center;">
-                      <span style="position:relative;" ><span style="color:red;margin-right:0px;position:absolute;left:-10px;top:0px;"></span>保全法院</span>
-                    </a-col>
-                    <a-col :span="8">
-                      <a-input v-model="element.court" readonly placeholder="请输入保全法院！" style="border: 0px solid #fefefe;  border-bottom: 1px solid #f0f0f0;"  />
-                    </a-col>
-                  </a-row>
-                </div>
-                </div>
-
-                <div class="reward-apply-content-item" style="margin-top:5px;margin-bottom:5px; margin-right:10px; display:none;">
-                  <a-row>
-                    <a-col :span="4" style="height:auto; font-size:1.0rem; margin-top:5px; text-align: center;">
-                      <span style="position:relative;" ><span style="color:red;margin-right:0px;position:absolute;left:-10px;top:0px;"></span>申请内容</span>
-                    </a-col>
-                    <a-col :span="20">
-                      <a-textarea
-                        v-model="element.content"
-                        placeholder="请输入申请内容！"
-                        :auto-size="{ minRows: 3, maxRows: 100 }"
-                        style="height:60px; border: 0px solid #fefefe;  border-bottom: 1px solid #f0f0f0;"
-                      />
+                      <a-date-picker v-model="element.appeal_time" placeholder="请输入上诉日期！" style="border: 0px solid #fefefe;  border-bottom: 1px solid #f0f0f0; width:100%; border-width: 0px 0px 1px; border-style: solid; border-color: rgb(254, 254, 254) rgb(254, 254, 254) rgb(240, 240, 240); border-image: initial;" :filter-option="filterOption" />
                     </a-col>
                   </a-row>
                 </div>
@@ -252,6 +152,9 @@
                       <div style="position:absolute; display:inline; float:left; margin-top:10px; " @click="downloadFiles(element)" >
                         <span>{{ element.fileName }}</span>
                       </div>
+                    </a-col>
+                    <a-col :span="24" style="font-size:1.0rem; margin-top:5px; text-align: center;">
+                      <span style="position:absolute; left:5.5rem; top:0.25rem; text-algin:left; color:red; font-size:12px; ">注：请上传法律文书、判决书、裁判书、收取凭证等相关资料！</span>
                     </a-col>
                   </a-row>
                 </div>
