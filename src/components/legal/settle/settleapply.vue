6257094d9f10d7355a1bc0047646d994c5ee1cb1
yunwisdom <qq695926920@163.com>
2021-09-07 16:35:52 +0800

    init

--- src/components/legal/settle/settleapply.vue
+++ src/components/legal/settle/settleapply.vue
@@ -160,7 +160,7 @@
                       </div>
                     </a-col>
                     <a-col :span="24" style="font-size:1.0rem; margin-top:5px; text-align: center;">
-                      <span style="position:absolute; left:5.5rem; top:0.25rem; text-algin:left; color:red; font-size:12px; ">注：请上传保全相关资料（如：保函、文书、保全资料等）！</span>
+                      <span style="position:absolute; left:5.5rem; top:0.25rem; text-algin:left; color:red; font-size:12px; ">注：请上传结案相关资料！</span>
                     </a-col>
                   </a-row>
                 </div>
